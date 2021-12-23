f4fafe2cb9c6599449b840e14c474f2cf7a07d1d
Vadim Mashuk <mashukw@gmail.com>
2021-12-23 19:11:04 +0100

    I am ready

--- Services/Mango.Services.CouponAPI/Services/CouponService.cs
+++ Services/Mango.Services.CouponAPI/Services/CouponService.cs
@@ -24,6 +24,8 @@ namespace Mango.Services.CouponAPI.Services
         public async Task<Result<CouponApi>> GetByCode(string couponCode)
         {
             // This is code from branch test 1
+            // This is code from branch test 2
+            // This is code from branch test 3
 
             var coupon = await _couponRepository.Query().FirstOrDefaultAsync(x => x.Code == couponCode);
             return _mapper.Map<CouponApi>(coupon);
